8f6c691018cef70412c92f92522084059093b48c
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-27 20:21:20 -0800

    add test to the nav components

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import {render, cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 afterEach(cleanup);
 
@@ -13,5 +12,6 @@ describe('About component', () =>{
     it('matches snapshot DOM node structrure', () => {
         const {asFragment} = render(<About />);
         expect(asFragment()).toMatchSnapshot();
-    })
-})
+    });
+});
+
